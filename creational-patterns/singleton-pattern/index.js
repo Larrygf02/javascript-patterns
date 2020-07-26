b173b93fb8ba04879acd84749e6a525d36b8f493
Larrygf02 <raulgf_02@hotmail.com>
2020-07-25 23:55:05 -0500

    singlenton pattern

---
+++ creational-patterns/singleton-pattern/index.js
@@ -0,0 +1,30 @@
+// singleton es un patron de diseño
+// donde solo puede existir una instancia
+// de una clase
+
+class Database {
+    constructor(data) {
+        if (Database.exists) {
+            return Database.instance
+        }
+        this._data = data;
+        Database.instance = this
+        Database.exists = true
+        return this;
+    }
+    getData() {
+        return this._data;
+    }
+    setData(data) {
+        this._data = data;
+    }
+}
+
+const mongo = new Database('mongo')
+console.log(mongo.getData())
+
+const othermongo = new Database('mongo')
+console.log(mongo == othermongo)
+
+const mysql = new Database('mysql')
+console.log(mongo == mysql)
