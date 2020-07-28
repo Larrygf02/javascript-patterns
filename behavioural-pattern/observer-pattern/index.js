16c2cfba18cfbf031f03bca8f2de7de2092023d4
Larrygf02 <raulgf_02@hotmail.com>
2020-07-27 22:25:47 -0500

    Implement patron observer

--- behavioural-pattern/observer-pattern/index.js
+++ behavioural-pattern/observer-pattern/index.js
@@ -14,3 +14,36 @@ class Subject {
         })
     }
 }
+
+class Observer {
+    constructor(state) {
+        this.state = state;
+        this.initialState = state;
+    }
+    update(change) {
+        let state = this.state;
+        switch(change) {
+            case 'INC':
+                this.state = ++state;
+                break;
+            case 'DEC':
+                this.state = --state;
+                break;
+            default:
+                this.state = this.initialState;
+        }
+    }
+}
+
+const sub = new Subject()
+
+const obs1 = new Observer(1)
+const obs2 = new Observer(19)
+
+sub.subscribe(obs1)
+sub.subscribe(obs2)
+
+sub.fire('INC')
+
+console.log(obs1.state)
+console.log(obs2.state)
